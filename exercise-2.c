8c8ba43078c89887bfb4ab3f850963a193b6b3a8
fernandoperez3108 <fernandoperezfloresjr@gmail.com>
2023-10-05 16:55:45 -0400

    feat: add exercise-2.c

---
+++ exercise-2.c
@@ -0,0 +1,28 @@
+/*
+2. hacer una funcion que diga si un caracter esta en un cadena 
+*/
+
+#include <stdio.h> 
+#include <string.h>
+
+int main() {
+    char cadena [100], caracter;
+    int i, cont = 0;
+
+    printf("digite una cadena: ");
+    gets(cadena);
+    printf("digite un caracter");
+    scanf("%c", &caracter);
+
+    for(i = 0; i < strlen(cadena); i++){
+        if (cadena[i] == caracter) {
+            cont++;
+        }
+    }
+
+    if (cont > 0) {
+        printf("el caracter %c esta en la cadena %s\n", caracter, cadena);
+    } else {
+        printf("el caracter %c no esta en la cadena %s\n", caracter, cadena);
+    }
+}
